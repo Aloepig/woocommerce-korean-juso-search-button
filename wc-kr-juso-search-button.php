94e34b4e4e50589d0264e8f4b4d8d63659e2e7d8
aloepig <aloepig@gmail.com>
2019-05-31 18:02:43 +0900

    개발중

--- wc-kr-juso-search-button.php
+++ wc-kr-juso-search-button.php
@@ -25,7 +25,13 @@ function wc_juso_button_scripts(){
 // woocommerce : 주소검색 버튼
 function jh_juso_search_button() {
     ?>
-    <button id="jh_juso_search_button">주소 검색</button>
+    <input type="text" id="jh_juso_search_text">
+    <button type="button" id="jh_juso_search_button" onclick="getAddr();">우편번호 검색</button>
+    <div id=jh_juso_list></div>
+    <script>
+   // jQuery("jh_juso_search_button").click(getAddr());
+    function jusoSearch() {}
+    </script>
     <?php
 }
 
@@ -39,10 +45,23 @@ add_filter('woocommerce_checkout_fields', 'custom_override_checkout_fields');
 //add_filter('woocommerce_shipping_fields', 'custom_override_checkout_fields');
 // Our hooked in function - $fields is passed via the filter!
 function custom_override_checkout_fields( $fields ) {
-    $fields['order']['order_comments']['placeholder'] = 'My new placeholder';
-    $fields['order']['order_comments']['type'] = 'password';
-    $fields['order']['order_comments']['label'] = 'My new label';
+    // $fields['order']['order_comments']['placeholder'] = 'My new placeholder';
+    // $fields['billing']['billing_postcode']['clear'] = true;
+    // $fields['order']['order_comments']['label'] = 'My new label';
+    // $fields['shipping']['shipping_phone'] = array(
+    //     'label'     => __('Phone', 'woocommerce'),
+    // 'placeholder'   => _x('Phone', 'placeholder', 'woocommerce'),
+    // 'required'  => false,
+    // 'class'     => array('form-row-wide'),
+    // 'clear'     => true
+    // );
     return $fields;
+}
+
+add_filter('woocommerce_billing_fields', 'my_woocommerce_billing_fields'); 
+function my_woocommerce_billing_fields($fields) { 
+    $fields['billing_postcode']['custom_attributes'] = array('readonly'=>'readonly');
+ return $fields; 
 }
 
 ?>
