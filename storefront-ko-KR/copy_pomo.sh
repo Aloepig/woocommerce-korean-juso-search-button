58b704d6b951696a900cfd162422117a9dacaae0
aloepig <aloepig@gmail.com>
2019-07-08 10:36:37 +0900

    storefront-ko-KR

---
+++ storefront-ko-KR/copy_pomo.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+echo "Storefront po & mo 파일복사"
+echo "실행명령: sh copy_pomo.sh"
+echo "/wp-content/themes/storefront/inc / woocommerce / storefront-woocommerce-template-functions.php 텍스트를 번역"
+cp storefront-ko_KR.mo /usr/share/nginx/html/jangheungdawon/wp-content/themes/storefront/storefront-ko_KR.mo
+cp storefront-ko_KR.po /usr/share/nginx/html/jangheungdawon/wp-content/themes/storefront/storefront-ko_KR.po
+echo "wp-content/themes/storefront 복사완료"
