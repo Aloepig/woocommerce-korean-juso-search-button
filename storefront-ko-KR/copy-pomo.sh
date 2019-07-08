34e928e25913b7dfd35b174904b7b518ec5c1dfc
aloepig <aloepig@gmail.com>
2019-07-08 10:50:09 +0900

    storefront-ko-KR  생성

--- storefront-ko-KR/copy-pomo.sh
+++ storefront-ko-KR/copy-pomo.sh
@@ -2,6 +2,6 @@
 echo "Storefront po & mo 파일복사"
 echo "실행명령: sh copy_pomo.sh"
 echo "/wp-content/themes/storefront/inc/woocommerce/storefront-woocommerce-template-functions.php 텍스트 번역을 위해"
-cp storefront-ko_KR.mo /usr/share/nginx/html/jangheungdawon/wp-content/themes/storefront/languages/storefront-ko_KR.mo
-cp storefront-ko_KR.po /usr/share/nginx/html/jangheungdawon/wp-content/themes/storefront/languages/storefront-ko_KR.po
-echo "wp-content/themes/storefront/languages 복사완료"
+cp storefront-ko_KR.mo /usr/share/nginx/html/jangheungdawon/wp-content/languages/themes/storefront-ko_KR.mo
+cp storefront-ko_KR.po /usr/share/nginx/html/jangheungdawon/wp-content/languages/themes/storefront-ko_KR.po
+echo "wp-content/languages/themes 복사완료"
